5b1b39252086926d387779f5e087cc76c03197ac
魏国兵 <weiguobing@meituan.com>
2018-05-03 08:41:20 +0800

    数据库查询

--- src/main/java/com/autocar/intelligent/hardware/service/impl/HardwareUploadServiceImpl.java
+++ src/main/java/com/autocar/intelligent/hardware/service/impl/HardwareUploadServiceImpl.java
@@ -1,18 +1,25 @@
 package com.autocar.intelligent.hardware.service.impl;
 
+import com.autocar.intelligent.hardware.dao.groovy.HardwareUploadMapper;
 import com.autocar.intelligent.hardware.domain.model.CarIntelligentHardwareUploadModel;
 import com.autocar.intelligent.hardware.service.HardwareUploadService;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * Created by guobingwei on 18/4/29.
  */
+@Service
 public class HardwareUploadServiceImpl implements HardwareUploadService {
+
+    @Resource
+    private HardwareUploadMapper hardwareUploadMapper;
 
     @Override
     public List<CarIntelligentHardwareUploadModel> getList() {
-        return null;
+        return hardwareUploadMapper.getList();
     }
 
     @Override
--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `car_intelligent_hardware_upload` (
-  `id` unsigned int auto_increment PRIMARY KEY ,
-  `temperature` DOUBLE(8, 4) NOT NULL COMMENT '温度',
+CREATE TABLE if not exists `car_intelligent_hardware_upload` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT primary key COMMENT '自增id',
+  `temperatures` DOUBLE(8, 4) NOT NULL COMMENT '温度',
   `back_distance` DOUBLE(8, 4) NOT NULL COMMENT '倒车距离',
-  `mtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `mtime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `ctime` datetime NOT NULL COMMENT '创建时间'
-)engine=innodb default charset=utf8mb4 COMMENT = '汽车智能硬件数据上报表';
+)engine=innodb default charset=utf8mb4 COMMENT = '汽车智能硬件数据上报表'
