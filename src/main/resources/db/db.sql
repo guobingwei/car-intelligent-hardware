6f3ad35abca3b80c2719af18ab4fca6f88ce7abe
weiguobing <weiguobing@meituan.com>
2020-04-05 18:32:05 +0800

    说明

--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -3,5 +3,5 @@ CREATE TABLE if not exists `car_intelligent_hardware_upload` (
   `temperatures` DOUBLE(8, 4) NOT NULL COMMENT '温度',
   `back_distance` DOUBLE(8, 4) NOT NULL COMMENT '倒车距离',
   `mtime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  `ctime` datetime NOT NULL COMMENT '创建时间'
+  `ctime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
 )engine=innodb default charset=utf8mb4 COMMENT = '汽车智能硬件数据上报表'
